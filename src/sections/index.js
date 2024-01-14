12bdf2c8feecb2c89c7fb3f4819740e706d5cd33
stretchfps <stretchvalo@gmail.com>
2024-01-14 18:31:52 +0530

    Initial Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './Specialoffer';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import Services from './Services';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+
+export{
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    Footer,
+    Services,
+    CustomerReviews
+}
